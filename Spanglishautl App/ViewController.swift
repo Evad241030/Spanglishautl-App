2f2c8d87a8eac417e4b60e93ff31cada5614ba05
davismony@me.com <davismony@me.com>
2015-11-29 09:21:34 -0800

    Initial Commit

---
+++ Spanglishautl App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spanglishautl App
+//
+//  Created by DAVID GONZALEZ on 11/29/15.
+//  Copyright © 2015 David Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
