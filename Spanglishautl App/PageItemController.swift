b5cb114840dc2879e12c2f9aeff38c6231e305de
davismony@me.com <davismony@me.com>
2016-02-11 01:14:50 -0800

    Set up all Multiple Choices

--- Spanglishautl App/PageItemController.swift
+++ Spanglishautl App/PageItemController.swift
@@ -11,7 +11,14 @@ import UIKit
 class PageItemController: UIViewController {
 
     var itemIndex: Int = 0
-    var correctAnswer: String?
+    var correctAnswer: Int?
+    
+    
+    let normalNames = ["Spanglishuatl", "Bean", "Cacao", "Star", "Xocolatl", "Cord", "Mesquite2", "Kernel", "Mahiz", "Ribbed", "Tamalli", "Asterisk", "Nopalli", "Seed", "Chian", "Pear", "Ahuacatl", "Teardrop", "Tomatl", "Reuleaux Triangle", "Chilli", "Circles", "Ahuacamolli", "Blank"]
+    
+    let possibleAnswers = ["Spanglishuatl", "Bean", "Cacao", "Star", "Xocolatl", "Cord", "Mesquite2", "Kernel", "Mahiz", "Ribbed", "Tamalli", "Asterisk", "Nopalli", "Seed", "Chian", "Pear", "Ahuacatl", "Teardrop", "Tomatl", "Reuleaux Triangle", "Chilli", "Circles", "Ahuacamolli", "Blank"]
+    
+    
     
     @IBOutlet weak var buttonA: UIButton!
     @IBOutlet weak var buttonB: UIButton!
@@ -30,27 +37,95 @@ class PageItemController: UIViewController {
         }
     }
     
+    
+    
     @IBOutlet var contentImageView: UIImageView?
     @IBOutlet weak var QuestionLabel: UILabel!
     @IBAction func buttonAhit(sender: UIButton) {
+        if(correctAnswer) == 1 {
+            responseLabel.text = "Correct."
+        } else {
+            responseLabel.text = "Wrong. Try again."
+        }
     }
     @IBAction func buttonBhit(sender: UIButton) {
+        if(correctAnswer) == 2 {
+            responseLabel.text = "Correct."
+        } else {
+            responseLabel.text = "Wrong. Try again."
+        }
     }
     @IBAction func buttonChit(sender: UIButton) {
+        if(correctAnswer) == 3 {
+            responseLabel.text = "Correct."
+        } else {
+            responseLabel.text = "Wrong. Try again."
+        }
     }
     @IBAction func buttonDhit(sender: UIButton) {
+        if(correctAnswer) == 4 {
+            responseLabel.text = "Correct."
+        } else {
+            responseLabel.text = "Wrong. Try again."
+        }
     }
     @IBOutlet weak var responseLabel: UILabel!
     
     func assignAnswer() {
-        buttonA.setTitle(imageName, forState: UIControlState.Normal)
-    }
+        
+        var RandomNumber = arc4random() % 4
+        RandomNumber += 1 // Don't like starting with zero
+        
+        switch(RandomNumber) {
+            
+        case 1:
+            buttonA.setTitle("A. \(imageName)", forState: UIControlState.Normal)
+            buttonB.setTitle("Helium", forState: UIControlState.Normal)
+            buttonC.setTitle("Lithium", forState: UIControlState.Normal)
+            buttonD.setTitle("Beryllium", forState: UIControlState.Normal)
+            correctAnswer = 1
+            break
+            
+        case 2:
+            buttonA.setTitle("Helium", forState: UIControlState.Normal)
+            buttonB.setTitle("B. \(imageName)", forState: UIControlState.Normal)
+            buttonC.setTitle("Lithium", forState: UIControlState.Normal)
+            buttonD.setTitle("Beryllium", forState: UIControlState.Normal)
+            correctAnswer = 2
+            break
+            
+        case 3:
+            buttonA.setTitle("Lithium", forState: UIControlState.Normal)
+            buttonB.setTitle("Helium", forState: UIControlState.Normal)
+            buttonC.setTitle("C. \(imageName)", forState: UIControlState.Normal)
+            buttonD.setTitle("Beryllium", forState: UIControlState.Normal)
+            correctAnswer = 3
+            break
+            
+        case 4:
+            buttonA.setTitle("Beryllium", forState: UIControlState.Normal)
+            buttonB.setTitle("Helium", forState: UIControlState.Normal)
+            buttonC.setTitle("Lithium", forState: UIControlState.Normal)
+            buttonD.setTitle("D. \(imageName)", forState: UIControlState.Normal)
+            correctAnswer = 4
+            break
+            
+        default:
+            break
+        }
+        
+        
+//        buttonA.setTitle(imageName, forState: UIControlState.Normal)
+        }
     
     // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        QuestionLabel.text = "What shape is this?"
+        QuestionLabel.text = "What shape is \(imageName)?"
         contentImageView!.image = UIImage(named: imageName)
+//        itemIndex = 
+        
+        
         assignAnswer()
     }
 
