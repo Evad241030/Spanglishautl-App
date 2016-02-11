5b3703a59523872778664940e99ad0195cdeb7a8
davismony@me.com <davismony@me.com>
2016-02-10 22:09:12 -0800

    Add Buttons - First button is responding. Layout work in progress.

--- Spanglishautl App/PageItemController.swift
+++ Spanglishautl App/PageItemController.swift
@@ -11,6 +11,14 @@ import UIKit
 class PageItemController: UIViewController {
 
     var itemIndex: Int = 0
+    var correctAnswer: String?
+    
+    @IBOutlet weak var buttonA: UIButton!
+    @IBOutlet weak var buttonB: UIButton!
+    @IBOutlet weak var buttonC: UIButton!
+    @IBOutlet weak var buttonD: UIButton!
+    
+    
     var imageName: String = "" {
         
         didSet {
@@ -23,12 +31,27 @@ class PageItemController: UIViewController {
     }
     
     @IBOutlet var contentImageView: UIImageView?
+    @IBOutlet weak var QuestionLabel: UILabel!
+    @IBAction func buttonAhit(sender: UIButton) {
+    }
+    @IBAction func buttonBhit(sender: UIButton) {
+    }
+    @IBAction func buttonChit(sender: UIButton) {
+    }
+    @IBAction func buttonDhit(sender: UIButton) {
+    }
+    @IBOutlet weak var responseLabel: UILabel!
     
+    func assignAnswer() {
+        buttonA.setTitle(imageName, forState: UIControlState.Normal)
+    }
     
     // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        QuestionLabel.text = "What shape is this?"
         contentImageView!.image = UIImage(named: imageName)
+        assignAnswer()
     }
 
 
